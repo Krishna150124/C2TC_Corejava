b7d470be4b486cdad85c8479d1676e99cf14805f
USER <USER@DESKTOP-TSD6O2L>
2024-08-02 16:02:55 -0700

    dayfive

---
+++ Convarient/src/com/tns/dayseven/overriding/convarient/Color.java
@@ -0,0 +1,17 @@
+package com.tns.dayseven.overriding.convarient;
+
+public class Color {
+	protected Color getColor() {
+		Color s = new Color();
+		return s;
+	}
+}
+
+//this class will inherit from the class color and will override the getcolor() method returning the object of the type red class
+class Red extends Color {
+	@Override
+	public Red getColor() {
+		Red s = new Red();
+		return s;
+	}
+}
---
+++ com.tnsif.dayfive.association.hasa/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module com.tnsif.dayfive.association.hasa {
+}
