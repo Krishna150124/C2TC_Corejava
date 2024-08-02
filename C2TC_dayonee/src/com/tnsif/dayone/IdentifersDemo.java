3fab0b0ecab4f6f39e477879359bc55d9f94a895
USER <USER@DESKTOP-TSD6O2L>
2024-08-02 15:56:42 -0700

    hello

---
+++ C2TC_dayonee/src/com/tnsif/dayone/IdentifersDemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.dayone;
+
+public class IdentifersDemo {
+	public static void main(String[] args) {
+		int $number = 30;
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Aniket";
+		System.out.println("value is : "+studentName);
+	}
+
+}
